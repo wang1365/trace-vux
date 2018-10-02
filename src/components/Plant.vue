b1ac64aa42e545b621b483d56a34b3b37d5228d5
xiaochuan.wangxc <wangxiaochuan01@163.com>
2018-10-02 11:17:28 +0800

    add preview image for video play

--- src/components/Plant.vue
+++ src/components/Plant.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     为什么寿光蔬菜这么好呢？
-    <video style="height: 100%;width: 100%" controls>
+    <video style="height: 100%;width: 100%" controls poster="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=583902322,1013386853&fm=26&gp=0.jpg">
       <source src="http://ips.ifeng.com/video19.ifeng.com/video09/2018/04/19/25603882-102-9987625-025625.mp4">
     </video>
   </div>
