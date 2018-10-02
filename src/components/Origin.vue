6c05b8c41ac15881b49ce5a165b89d57b0106b8f
xiaochuan.wangxc <wangxiaochuan01@163.com>
2018-10-02 09:36:56 +0800

    add baidu map

--- src/components/Origin.vue
+++ src/components/Origin.vue
@@ -1,21 +1,52 @@
 <template>
   <div>
-    这里显示产地信息
+    山东省寿光市文家街道桑家村
+    <!--<TraceMap :height="height" :longitude="longitude" :latitude="latitude"/>-->
+    <div id="tracemap" ref="allmap"/>
   </div>
 
 </template>
 
 <script>
 import { Swiper } from 'vux'
+import BMap from 'BMap'
 
 export default {
   name: 'Origin',
   components: {
-    Swiper
+    Swiper,
+    BMap
   },
   data() {
     return {
+      map: null
     }
+  },
+  mounted() {
+    // this.ready()
+    this.$nextTick(this.ready())
+  },
+  methods: {
+    ready() {
+      const map = new BMap.Map('tracemap')
+      this.map = map
+      const point = new BMap.Point(132.1569, 43.87192)
+      map.setCurrentCity('潍坊')
+      map.centerAndZoom(point, 10)
+      map.addControl(new BMap.MapTypeControl())
+      map.enableScrollWheelZoom(true)
+      map.enableDoubleClickZoom(true)
+      const marker = new BMap.Marker(point)
+      map.addOverlay(marker)
+    }
+
   }
 }
 </script>
+
+<style scoped>
+  #tracemap {
+    height: 300px;
+    width: 100%;
+  }
+</style>
--- src/components/Quality.vue
+++ src/components/Quality.vue
@@ -1,36 +1,59 @@
 <template>
   <div>
-    这里显示质量检测报告信息
+    <div v-for="src in list" :key="src" style="background-color:yellow;text-align:center;">
+      <span style="font-size:20px;">Loading</span>
+      <x-img :src="src" :webp-src="`${src}?type=webp`" :offset="-100" class="ximg-demo" error-class="ximg-error" container="#vux_view_box_body" @on-success="success" @on-error="error"/>
+    </div>
   </div>
 
 </template>
 
 <script>
-import { Swiper } from 'vux'
-const baseList = [{
-  url: 'javascript:',
-  img: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vvsr72j20p00gogo2.jpg',
-  title: '高荣军'
-}, {
-  url: 'javascript:',
-  img: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vw1k2wj20p00goq7n.jpg',
-  title: '王晓川'
-}, {
-  url: 'javascript:',
-  img: 'https://static.vux.li/demo/5.jpg', // 404
-  title: '王晓森',
-  fallbackImg: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vw50iwj20ff0aaaci.jpg'
-}]
+import { XImg } from 'vux'
 
 export default {
   name: 'Quality',
   components: {
-    Swiper
+    XImg
   },
   data() {
     return {
-      demo01_list: baseList
+      list: [
+        'https://o5omsejde.qnssl.com/demo/test1.jpg',
+        'https://o5omsejde.qnssl.com/demo/test2.jpg',
+        'https://o5omsejde.qnssl.com/demo/test4.jpg',
+        'https://o5omsejde.qnssl.com/demo/test5.jpg',
+        'https://o5omsejde.qnssl.com/demo/test6.jpg',
+        'https://o5omsejde.qnssl.com/demo/test7.jpg',
+        'https://o5omsejde.qnssl.com/demo/test8.jpg'
+      ]
+    }
+  },
+  methods: {
+    success(src, ele) {
+      console.log('success load', src)
+      const span = ele.parentNode.querySelector('span')
+      ele.parentNode.removeChild(span)
+    },
+    error(src, ele, msg) {
+      console.log('error load', msg, src)
+      const span = ele.parentNode.querySelector('span')
+      span.innerText = 'load error'
     }
   }
 }
 </script>
+
+<style>
+  .ximg-demo {
+    width: 100%;
+    height: auto;
+  }
+  .ximg-error {
+    background-color: yellow;
+  }
+  .ximg-error:after {
+    content: '加载失败';
+    color: red;
+  }
+</style>
