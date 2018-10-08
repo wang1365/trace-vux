2a84b552c82f92de69e696899388e049cd1cac2a
xiaochuan.wangxc <wangxiaochuan01@163.com>
2018-10-08 19:03:40 +0800

    update

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.BASE_API, // api的base_url
-  timeout: 5000 // request timeout
+  timeout: 5000, // request timeout
+  withCredentials: true
 })
 
 // request interceptor
